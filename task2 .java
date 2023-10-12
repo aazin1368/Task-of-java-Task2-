38dd3f48f9c27591ce46380eb56daeccc7ea9ab8
aazin1368 <147614667+aazin1368@users.noreply.github.com>
2023-10-12 22:39:04 +0200

    Add files via upload

---
+++ task2 .java
@@ -0,0 +1,106 @@
+public class Car {
+    private int id;
+    private String make;
+    private String model;
+    private int yearOfManufacture;
+    private String color;
+    private double price;
+    private String registrationNumber;
+
+    public Car(int id, String make, String model, int yearOfManufacture, String color, double price, String registrationNumber) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.yearOfManufacture = yearOfManufacture;
+        this.color = color;
+        this.price = price;
+        this.registrationNumber = registrationNumber;
+    }
+
+    // Getters and setters for the Car class attributes
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", yearOfManufacture=" + yearOfManufacture +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        // Create an array of Car objects
+        Car[] cars = new Car[3];
+
+        // Initialize Car objects and add them to the array
+        cars[0] = new Car(1, "Benz", "C300", 2023, "White", 150000.0, "POI475984");
+        cars[1] = new Car(2, "Bmw", "520i", 2022, "Black", 98000.0, "TRE247651");
+        cars[2] = new Car(3, "Ferrari", "F60", 2023, "Orange", 890000.0, "BVC456321");
+
+        // Access and manipulate Car objects in the array
+        for (Car car : cars) {
+            System.out.println(car.toString());
+        }
+    }
+}
+
